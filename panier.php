6504fba3665102a9cc7417c050de80a61425631a
abouKore <abouchekou@gmail.com>
2022-12-17 22:00:32 +0100

    Version 1.1.2
    bdd mise a jour

--- panier.php
+++ panier.php
@@ -1,12 +1,16 @@
 <?php
-    if (!isset($_COOKIE["panier"])) { setcookie("panier", json_encode([])); }
-    if (isset($_GET["article"])) { $article = $_GET["article"]; }    
+if (!isset($_COOKIE["panier"])) {
+    setcookie("panier", json_encode([]));
+}
+if (isset($_GET["article"])) {
+    $article = $_GET["article"];
+}
 
-    $panier = json_decode($_COOKIE["panier"]);
-    array_push($panier, $article);
-    print_r($panier);
+$panier = json_decode($_COOKIE["panier"]);
+array_push($panier, $article);
+print_r($panier);
 
-    setcookie("panier", json_encode($panier));
+setcookie("panier", json_encode($panier));
 
-    // print_r(json_decode($_COOKIE["panier"]));
+// print_r(json_decode($_COOKIE["panier"]));
 ?>
