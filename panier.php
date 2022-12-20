4493716154612d4a8aa5496512c3f7bc161b1456
Nicolas ROBERT <nicolas.robert5@etu.univ-lorraine.fr>
2022-12-20 16:07:00 +0100

    ajout commentaires panier

--- panier.php
+++ panier.php
@@ -1,11 +1,14 @@
 <?php
 
-if (!isset($_COOKIE["panier"])) {
+if (!isset($_COOKIE["panier"])) { 
+    // si le cookie panier n'existe on le crée
     setcookie("panier", json_encode([]), 0, "/");
 
 } else {
     $panier = json_decode($_COOKIE["panier"]);
+
     if (isset($_GET["article"])) {
+        // ajout d'un nouvel article dans le panier
         $article = $_GET["article"];
 
         if (!in_array($article, $panier)) {
@@ -13,6 +16,7 @@ if (!isset($_COOKIE["panier"])) {
         }
         setcookie("panier", json_encode($panier));
     } else {
+        // affichage du contenu du panier
         echo "<ul>";
         foreach($panier as $aliment) {
             echo "<li><span class=\"aliment\">" . $aliment . "</span> <span class=\"supprimer\">❌</span></li>";
@@ -22,14 +26,12 @@ if (!isset($_COOKIE["panier"])) {
 }
 
 if (isset($_GET["supprimer"])) {
+    // suppression d'un article du panier
     $supprimer = $_GET["supprimer"];
     $panier = json_decode($_COOKIE["panier"]);
     foreach ($panier as $k => $v) {
-        print_r($panier); 
         echo "<br>";
         if ($v == $supprimer) {
-            echo $k . " => " . $v . " == "  . $supprimer . "<br>";
-
             $panier = array_splice($panier, $k+1, 1);
         }
         print_r($panier);
