3e97885734ba93271dc34d673b2b9b9cddfffe83
aboubacar <hassanec3u@etu.uni-lorraine.fr>
2022-12-23 18:15:40 +0100

    Page d'acceuil creer

---
+++ acceil.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+
+<head>
+    <title>Bienvenue sur StreetBar</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/1eeeff3f3f.js" crossorigin="anonymous"></script>
+
+</head>
+
+<header class="header">
+
+    <nav class="nav_enTete">
+        <div class="header_logo">
+            <img src="res/logo.jpg" height="100px" width="150px" alt="logo bar">
+        </div>
+        <div class="header_information">
+            <a href="acceil.php" class="header_information_item"><i class="fa-solid fa-magnifying-glass"></i> Rechercher</a>
+            <a href="connexion.php" class="header_information_item"><i class="fa-solid fa-user"></i> Compte</a>
+            <a href="panier.php" class="header_information_item"><i class="fa-solid fa-cart-shopping"></i> Panier</a>
+        </div>
+    </nav>
+</header>
+
+<body>
+
+</body>
+
+<footer>
+
+    <div class="footer_information">
+        <ul>
+            <li><a href="acceil.php" class="footer_information_item">Le copyright</li>
+            <li><a href="acceil.php" class="footer_information_item">Qui sommes-nous </a></li>
+            <li><a href="acceil.php" class="footer_information_item">Charte de confidentialite et Cookies </a></li>
+            <li><a href="acceil.php" class="footer_information_item">Condition general de vente </a></li>
+            <li><a href="acceil.php" class="footer_information_item">Information legales </a></li>
+            <li><a href="acceil.php" class="footer_information_item">La politique de confidentialité </a></li>
+            <li><a href="acceil.php" class="footer_information_item">Les conditions d'utilisation </a></li>
+        </ul>
+    </div>
+</footer>
+</html>
